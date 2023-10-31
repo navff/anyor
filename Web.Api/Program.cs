604a35755b0575779d7aa9f44819ddea168f8251
Варнавский Владимир Николаевич <vladimir.varnavskiy@mts.ru>
2023-10-31 06:43:43 +0300

    Add hangfire server

--- Web.Api/Program.cs
+++ Web.Api/Program.cs
@@ -14,6 +14,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddHangfire(c => c.UseMemoryStorage());
+builder.Services.AddHangfireServer();
 
 var app = builder.Build();
 
